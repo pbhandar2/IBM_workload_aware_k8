65417dfac292db86bf501f55c8b15bb19d27851d
root <root@ip-172-31-19-26.us-east-2.compute.internal>
2021-03-24 23:30:03 +0000

    more

---
+++ aws/install_docker.sh
@@ -0,0 +1,10 @@
+sudo apt-get update 
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
