8485990f48ad866b9b257340a8ca0f04dc35c161
root <root@ip-172-31-1-143.us-east-2.compute.internal>
2021-05-10 08:28:44 +0000

    add the replay generation file

--- experiments/fio/setup_workload/generate_replay_log.py
+++ experiments/fio/setup_workload/generate_replay_log.py
@@ -1,17 +1,16 @@
 import argparse 
+import pathlib 
 
 
-def main(latency_log_path, file_to_replay):
-    file_name = latency_log_path.name 
-    replay_log_file_name = "{}.log".format(file_name.split("_")[0])
-    replay_log_path = pathlib.Path("/mnt/data/replay_logs/").joinpath(replay_log_file_name)
+def main(latency_log_path, file_to_replay, output_file_name):
+    replay_log_path = pathlib.Path("/mnt/data/replay_logs/").joinpath(output_file_name)
 
     with replay_log_path.open("w+") as replay_log_handler:
 
         # header of the replay log 
         replay_log_handler.write("fio version 2 iolog\n")
-        replay_log_handler.write("{} add\n".format(read_file_path))
-        replay_log_handler.write("{} open\n".format(read_file_path))
+        replay_log_handler.write("{} add\n".format(file_to_replay))
+        replay_log_handler.write("{} open\n".format(file_to_replay))
 
         with latency_log_path.open("r") as latency_log_handler:
             latency_log_line = latency_log_handler.readline().rstrip()
@@ -38,6 +37,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Generate replay log based on the latency log.")
     parser.add_argument("latency_log_path", type=pathlib.Path, help="Path to latency log")
     parser.add_argument("file_to_replay", help="The path to the file to be reading in the replay log.")
+    parser.add_argument("output_file_name", help="The output file name, path is fixed (check code)")
     args = parser.parse_args()
 
-    main(args.latency_log_path, args.file_to_replay)
+    main(args.latency_log_path, args.file_to_replay, args.output_file_name)
